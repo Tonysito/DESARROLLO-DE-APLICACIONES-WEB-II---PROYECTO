bb25f802eccd6061202e70f04be7017498155f29
Anthony Aldair Espinoza Perez <anthonyaldairespinozaperez@outlook.com>
2025-10-12 20:51:06 +0200

    Actualizando proyecto

--- usuario-service/src/main/java/org/cibertec/controller/UsuarioController.java
+++ usuario-service/src/main/java/org/cibertec/controller/UsuarioController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
+//Api
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
