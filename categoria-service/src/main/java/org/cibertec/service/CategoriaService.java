12bf767b496ee75dcc33207858933c43ed86d83d
MiguelTFD <129565082+MiguelTFD@users.noreply.github.com>
2025-10-30 12:31:23 -0500

    Remove unnecessary blank lines in CategoriaService

--- categoria-service/src/main/java/org/cibertec/service/CategoriaService.java
+++ categoria-service/src/main/java/org/cibertec/service/CategoriaService.java
@@ -8,12 +8,9 @@ import org.cibertec.entity.Categoria;
 
 
 public interface CategoriaService {
-	
 	 List<Categoria> getAllCategorias();
 	 Optional<Categoria> getCategoriaById(Integer id);
 	 Categoria createCategoria(Categoria categoria);
 	 Categoria updateCategoria(Integer id, Categoria categoria); 
 	 void deleteCategoria(Integer id);
-  
-
 }
